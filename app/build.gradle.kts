7d96af7dd82a1e5e9484057d2186855f29dbf0e7
Menkaura81 <bertoalarcon@gmail.com>
2025-07-06 10:56:52 +0200

    Menu creado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,14 +29,20 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
-
+    implementation("androidx.navigation:navigation-ui:2.9.1")
+    implementation("androidx.navigation:navigation-fragment:2.9.1")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.fragment)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
